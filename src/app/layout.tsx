dc6f65cf4f1dd41fa4d8d19bf6dce0b80b5133a6
Emjaay20 <youngmj2010@gmail.com>
2026-03-01 16:43:19 +0100

    chore: adopt Next.js App Router icon.svg convention for Vercel deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,6 @@ import "./globals.scss";
 export const metadata: Metadata = {
   title: "Lendsqr",
   description: "Lendsqr frontend test",
-  icons: {
-    icon: "/icons/Union.svg",
-  },
 };
 
 export default function RootLayout({
